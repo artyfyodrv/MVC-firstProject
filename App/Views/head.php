806721a842f55dcac9dc9da1afcf018c548cefc5
artyfyodrv <aofyodrv@yandex.ru>
2023-02-03 22:20:35 +0300

    html+css header

--- App/Views/head.php
+++ App/Views/head.php
@@ -6,36 +6,72 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/../local/public/css/style.css" type="text/css">
-    <title><?= $title ?></title>
+    <link rel="stylesheet" href="/../local/public/css/navbar.css" type="text/css">
+    <title><? $title ?></title>
 </head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Project</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <!-- Toggle button -->
+        <button
+                class="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Главная</a>
-            </li>
-            <?php if (!empty($_SESSION['auth'])) { ?>
-            <li class="logout">
-                <a class="nav-link" href="/logout">Выход</a>
-            </li>
-            <li class="cabinet">
-                <a class="nav-link" href="/cabinet">Личный кабинет</a>
-            </li>
-            <?php } else { ?>
-            <li class="nav-item">
-                <a class="nav-link" href="/registration">Регистрация</a>
-            </li>
-            <li class="auth">
-                <a class="nav-link" href="/login">Авторизация</a>
-            </li>
-            <?php } ?>
-        </ul>
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Navbar brand -->
+            <a class="navbar-brand" href="#">Project</a>
+            <!-- Left links -->
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/info">Информация</a>
+                </li>
+            </ul>
+            <!-- Left links -->
+        </div>
+        <!-- Collapsible wrapper -->
+
+        <!-- Right elements -->
+        <div class="d-flex align-items-center">
+            <!-- Icon -->
+            <a class="link-secondary me-3" href="#">
+                <i class="fas fa-shopping-cart"></i>
+            </a>
+
+            <!-- Avatar -->
+            <div class="dropdown">
+                <ul class="navbar-nav mr-auto">
+                    <?php if (!empty($_SESSION['auth'])) { ?>
+                        <li class="logout">
+                            <a class="nav-link" href="/logout">Выход</a>
+                        </li>
+                        <li class="">
+                            <a class="nav-link" href="/cabinet"><?php echo $_SESSION['username'];?></a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/registration">Регистрация</a>
+                        </li>
+                        <li class="auth">
+                            <a class="nav-link" href="/login">Авторизация</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+        <!-- Right elements -->
     </div>
+    <!-- Container wrapper -->
 </nav>
+<!-- Navbar -->
