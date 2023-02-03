84077b8df3d50d66a398337050e3cf323f95f563
artyfyodrv <aofyodrv@yandex.ru>
2023-02-03 22:17:21 +0300

    release changepassword

--- App/Controllers/CabinetController.php
+++ App/Controllers/CabinetController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Entities\User;
+use App\Kernel\CabinetValidations;
 use App\Kernel\Connections\MySQLConnection;
 
 class CabinetController
@@ -10,7 +11,7 @@ class CabinetController
     static function cabinet()
     {
 
-     $title = "Личный кабинет";
+        $title = "Личный кабинет";
 
         $user = new User();
         $login = $_SESSION['login'];
@@ -18,15 +19,43 @@ class CabinetController
 
         $connect = MySQLConnection::getInstance();
         $result = $connect->getConnection()
-            ->query("SELECT user_name, email FROM users WHERE login='$login'");
+            ->query("SELECT user_name, email, password FROM users WHERE login='$login'");
         $row = mysqli_fetch_array($result);
 
         // запись в сессию данных с бд
         $_SESSION['username'] = $row["user_name"];
         $_SESSION['email'] = $row["email"];
 
-     include_once __DIR__ . "/../Views/cabinet.php";
+        include_once __DIR__ . "/../Views/cabinet.php";
 
 
+    }
+
+    public function changePassword()
+    {
+
+
+        $errorsCabinet = [];
+
+        $objValidations = new CabinetValidations();
+
+        $password = $_POST['password'];
+        $newpassword = $_POST['newpassword'];
+        $passconfirm = $_POST['passconfirm'];
+        $username = $_SESSION['username'];
+
+        $errorsCabinet = $objValidations->validateForms($password, $newpassword, $passconfirm,);
+
+        if (empty($errorsCabinet)) {
+            $connect = MySQLConnection::getInstance();
+            $result = $connect->getConnection()
+                ->query("UPDATE users SET password='$newpassword' WHERE user_name='$username'");
+        }
+
+        var_dump($errorsCabinet) ;
+        var_dump($password);
+
+
+        include_once __DIR__ . "/../Views/cabinet.php";
     }
 }
--- App/Views/cabinet.php
+++ App/Views/cabinet.php
@@ -1,15 +1,33 @@
+<title> <?php $title ?> </title>
+<?php include_once "head.php" ?>
 <?php
+    if (!empty($_SESSION['auth'])) { ?>
+        <div class="content-user" style="text-align: center">
+            <h4>Добро пожаловать<br> </h4><h1><?php echo $_SESSION['username']; ?></h1><br></h4>
+        </div>
+        <div class="content-settings" style="text-align: right; float:right; " >
+            <div class="passchangetext" style="text-align: center;"> <h4>Смена пароля: </h4>
+        </div>
+            <div class="input-settings" style="width: 400px; margin-right: 30px;">
+                <form action="/cabinet" method="POST">
+                <input type="text" class="form-control" name="password" id="password" placeholder="Введите ваш пароль"> <br>
+                <input type="text" class="form-control" name="newpassword" id="newpassword" placeholder="Введите новый пароль"> <br>
+                <input type="text" class="form-control" name="passconfirm" id="passconfirm" placeholder="Подтвердите новый пароль"> <br>
+                <button type="submit" class="btn btn-success" style="float: left; width: 400px;">Сменить пароль</button>
+                </form>
+            </div>
+            <?php if (empty($errorsCabinet) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+                echo 'Смена пароля успешна';
+            } else {
+                if (!empty($errorsCabinet)) {
+                    foreach ($errorsCabinet as $value) {
+                        echo "$value<br>";
+                    }
+                }
+            } ?>
+        </div>
+    <?php } ?>
 
-include_once "head.php" ?>
-<?php
-    if (!empty($_SESSION['auth'])) {
-        echo "Добро пожаловать: " . $_SESSION['username'];
-        echo "Ваша почта: " . $_SESSION['email'];
-    } else {
-        echo "NE HELLO";
-    }?>
-    <div class="container mt-4"?
-            <div style="text-align: center;"><label>Личный кабинет</label></div>
-    </div>
+
 
 <?php include_once "footer.php" ?>
